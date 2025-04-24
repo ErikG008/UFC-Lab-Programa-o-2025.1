9ba69a8f55e4c8e2b7713a79c94527729140526a
Erik <152098698+ErikG008@users.noreply.github.com>
2025-04-24 18:12:12 -0300

    Create Questão 4.c

---
+++ Lista 01/Questão 4.c
@@ -0,0 +1,11 @@
+//4-faça um programa em C que imprima o produto dos valores 30 e 27
+
+#include <stdio.h>
+
+int main() {
+
+int Product = 30 * 27;
+printf("Seu produto e: %d\n", Product);
+
+    return 0;
+}
