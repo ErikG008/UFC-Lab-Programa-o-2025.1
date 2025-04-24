83ff6c702037744d63209c240e1f58db3477714c
Erik <152098698+ErikG008@users.noreply.github.com>
2025-04-24 18:20:26 -0300

    Create questão 05.c

---
+++ Lista 01/questão 05.c
@@ -0,0 +1,15 @@
+//5-programa c que imprima media aritmetica entre 5, 8 e 12.
+#include <stdio.h>
+
+int main() {
+
+float A = 5;
+float B = 8;
+float C = 12;
+float MDA;
+
+MDA = (A + B + C) / 3;
+printf("Media Aritmetica eh: %2.f\n", MDA);
+
+    return 0;
+}
