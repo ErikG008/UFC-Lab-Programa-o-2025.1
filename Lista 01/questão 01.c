2ba5e3e601973f2ff9f0fdc129449d7e6994378b
Erik <152098698+ErikG008@users.noreply.github.com>
2025-04-24 16:08:17 -0300

    Add files via upload

---
+++ Lista 01/questão 01.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    
+float A, B, C, MEDIA;
+
+printf("Valor de A:");
+scanf("%f", &A);
+
+printf("Valor de B:");
+scanf("%f", &B);
+
+printf("Valor de C:");
+scanf("%f", &C);
+
+MEDIA = (A + B + C) / 3;
+printf("Media e: %f\n", MEDIA);
+
+    return 0;
+}
